795123a574f43c811591bd9fdc3d7ca89e234fcd
isabellabarcelos <isabellabarcelossg@gmail.com>
2023-02-06 10:49:42 -0300

    first commit

---
+++ store/store_service.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	/*"strings"
+	"github.com/isabellabarcelos/url-shortener-2/handler"
+	// "github.com/isabellabarcelos/url-shortener-2/store"*/
+)
+
+var (
+	LinkList map[string]string
+)
+
+// SaveUrlMapping - Add a link to the linkList and generate a shorter link
+func SaveUrlMapping(LongLink string) {
+
+	_, ok := LinkList[LongLink] 
+	if ok {
+		fmt.Println("Already have this link")
+		return
+	}
+	
+	//LinkList[LongLink] = handler.GenerateUrl(LongLink)	
+	return
+	
+}
+
+// GetURL - Find link that matches the shortened link in the linkList
+//func getLink(ShortLink string) {
+//	i:=0
+//	for key, value := range LinkList {
+	//	if value == ShortLink{
+	//		return 
+	//	}
+	//	i++
+//	}
+	
+//	LinkList[ShortLink] = handler.GenerateUrl(LongLink)	//mapeia
+														//redireciona
+
+	
