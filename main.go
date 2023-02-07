ca193283fb0199e2a75be66ef4ce4aa3aefd87b0
Arthur Silva <arxdsilva@gmail.com>
2023-02-07 11:24:28 -0300

    convert to REST and JSON format

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
@@ -8,6 +9,12 @@ import (
 	"strings"
 	"time"
 )
+
+// API rest - JSON
+type ShortenedURL struct {
+	OriginalURL  string `json:"original_url"`
+	ShortenedURL string `json:"shortened_url"`
+}
 
 var (
 	linkList map[string]string
@@ -32,11 +39,17 @@ func CreateUrl(w http.ResponseWriter, r *http.Request) {
 		if _, ok := linkList[key[0]]; !ok {
 			genString := fmt.Sprint(rand.Int63n(1000))
 			linkList[genString] = key[0]
-			w.Header().Set("Content-Type", "text/html")
-			w.WriteHeader(http.StatusAccepted)
-			linkString := fmt.Sprintf("<a href=\"http://localhost:8080/short/%s\">http://localhost:8080/short/%s</a>", genString, genString)
-			fmt.Fprintf(w, "Added shortlink\n")
-			fmt.Fprintf(w, linkString)
+			short := ShortenedURL{
+				OriginalURL:  key[0],
+				ShortenedURL: fmt.Sprintf("http://localhost:8080/short/%s", genString),
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			if err := json.NewEncoder(w).Encode(short); err != nil {
+				fmt.Printf("error: %v\n", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
 			return
 		}
 		w.WriteHeader(http.StatusConflict)
@@ -46,7 +59,6 @@ func CreateUrl(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusBadRequest)
 	fmt.Fprintf(w, "Failed to add link")
-	return
 }
 
 func GetUrl(w http.ResponseWriter, r *http.Request) {
@@ -54,6 +66,4 @@ func GetUrl(w http.ResponseWriter, r *http.Request) {
 	pathArgs := strings.Split(path, "/")
 	log.Printf("Redirected to: %s", linkList[pathArgs[2]])
 	http.Redirect(w, r, linkList[pathArgs[2]], http.StatusPermanentRedirect)
-	return
 }
-
